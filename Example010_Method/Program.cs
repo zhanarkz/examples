582a1a0f0b4b82d47f6334b97de19e3a7c165eb4
Zhanar <zhanarkz@gmail.com>
2022-08-11 23:06:04 +0600

    Example10

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {10, 25, 13, 43, 53, 65, 76, 84,10};
+
+int n = array.Length;
+int find = 10;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index+1;
+    index++;
+}
