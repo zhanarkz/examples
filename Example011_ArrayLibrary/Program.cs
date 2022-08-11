1529bd4ea29bdd48a657e394a39fdae34a11e1b1
Zhanar <zhanarkz@gmail.com>
2022-08-11 23:06:39 +0600

    example11

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,54 @@
+﻿// new int [] - create new massive where N elements
+// void FillArray - метод заполнения массива
+// void PrintArray - метод выведения на экран массива
+void FillArray(int [] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 15);
+        //index = index +1; 
+        index++;
+    }
+}
+
+void PrintArray(int [] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[ position ]);
+        position++ ;
+    }
+}
+
+int Indexof(int [] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+    while (index < count)
+    {
+        if (collection[index] == find)
+    {
+        position = index;
+        break;
+    }
+    index++;
+    }
+    return position;
+}
+
+int [] array = new int [10];
+
+FillArray(array);
+array[5] = 5;
+array[9] = 5;
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = Indexof(array, 5);
+Console.WriteLine(pos);
